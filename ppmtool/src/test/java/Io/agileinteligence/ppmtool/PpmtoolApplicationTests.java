40b9aaced496e9cacc58dcc73302adec178084ad
YounesHarir <47776050+YounesHarir@users.noreply.github.com>
2020-07-20 23:29:32 +0100

    first commit

---
+++ ppmtool/src/test/java/Io/agileinteligence/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package Io.agileinteligence.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
